4e515df93ee2184ee8b60969147db3fe854191ab
Louis Gadza <siuolgadza@gmail.com>
2023-01-28 14:25:13 +0100

    tournament model

--- src/api/tournaments/model.js
+++ src/api/tournaments/model.js
@@ -7,6 +7,60 @@ export const tournamentsSchema = new Schema(
     name: { type: String, required: true },
     discipline_cover: { type: String },
     discipline_name: { type: String },
+    registration: {
+      activation: {
+        isRegistration: { type: Boolean },
+        registrationOpeningDate: { type: String },
+        registrationClosingDate: { type: String },
+      },
+      options: {
+        isRegistrationAutomatically: { type: Boolean },
+        isEmailNotificationAutomatically: { type: Boolean },
+      },
+      customization: {
+        validationMessage: { type: String },
+        refusalMessage: { type: String },
+      },
+    },
+    structure: {
+      general: {
+        number: { type: Number },
+        size: { type: Number },
+        numberOfDivisions: { type: Number },
+        name: { type: String },
+      },
+      advanced: {
+        groupComposition: { type: String },
+        pointsAtrribution: {
+          win: { isWin: { type: Boolean }, points: { type: Number } },
+          draw: { isDraw: { type: Boolean }, points: { type: Number } },
+          lost: { isLost: { type: Boolean }, points: { type: Number } },
+        },
+        matchForfeit: {
+          isForfeit: { type: Boolean },
+          points: { type: Number },
+        },
+      },
+      tiebreaker: {
+        option1: { type: String },
+        option2: { type: String },
+        option3: { type: String },
+      },
+      placement: {
+        isPlacement: { type: Boolean },
+      },
+      matchSettings: {
+        option: { type: String },
+      },
+    },
+    participants: {
+      isCheck_in: { type: Boolean },
+      checkInOpeningDate: { type: String },
+      checkInClosingDate: { type: String },
+    },
+    discipline_name: { type: String },
+    discipline_name: { type: String },
+    discipline_name: { type: String },
     platform: { type: String, required: true },
     size: { type: Number, required: true },
   },
