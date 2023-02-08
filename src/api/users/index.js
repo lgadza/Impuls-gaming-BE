9bf64f7fc3958c127a152767db792005f24fd93d
Louis Gadza <siuolgadza@gmail.com>
2023-02-09 00:19:26 +0100

    registerAPI fixed

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -9,12 +9,13 @@ import UsersModel from "./model.js";
 
 const usersRouter = express.Router();
 
-usersRouter.post("/register ", async (req, res, next) => {
+usersRouter.post("/register", async (req, res, next) => {
   try {
     const newUser = new UsersModel(req.body);
     const { _id } = await newUser.save();
     res.status(201).send({ _id });
   } catch (error) {
+    console.log(error);
     next(error);
   }
 });
