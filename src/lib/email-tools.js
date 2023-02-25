2a3ec87f4e8786a924c5c795e206133c6b63014c
Louis Gadza <siuolgadza@gmail.com>
2023-02-25 10:24:48 +0100

    fix email

--- src/lib/email-tools.js
+++ src/lib/email-tools.js
@@ -13,18 +13,21 @@ export const sendRegistrationEmail = async (
     from: process.env.SENDER_EMAIL,
     subject: ` Verify Your Email Address`,
 
-    html: `<div><strong>Dear ${userData.name} ${userData.surname} </strong>
+    html:
+      `<div><strong>Dear ${userData.name} ${userData.surname} </strong>
     <div>
     <span>Thank you for signing up for our service. To ensure the security of your account and the accuracy of your information, we need to verify your email address.
 
 To complete the verification process, please click on the button below:
 
-<div><button><a href="https://impulsgaming.vercel.app/organizer/verifyEmail/${userId}">verify email</a></button></div>
+<div><button><a href=https://impulsgaming.vercel.app/organizer/verifyEmail/${userId}` /
+      `${userData.name}">verify email</a></button></div>
 
 If you are unable to click the button, please copy and paste the link below  into your browser's address bar.
-
-<a href="https://impulsgaming.vercel.app/organizer/verifyEmail/${userId}">verify email</a>
-
+</br>
+<span {"https://impulsgaming.vercel.app/organizer/verifyEmail/${userId}` /
+      `${userData.name}"}></span>
+</br>
 Please note that your account will remain inactive until you complete the verification process. If you have any questions or concerns, please feel free to contact us.
 
 <a href="tel:794144892">0794-144-892</a>
